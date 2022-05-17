fcc9f91c9de0c959541d2b381bc93eb7ec7bd866
JeonSeungh0 <91254011+JeonSeungh0@users.noreply.github.com>
2022-05-17 18:35:08 +0900

    Add files via upload

---
+++ vis/vis_md02/js/vis_md02.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.main_visual_box .text_wr .tit span').eq(0).addClass('active2');
+	setTimeout(function(){
+	$('.main_visual_box .text_wr .tit span').eq(1).addClass('active2');
+    }, 150);
+	setTimeout(function(){
+	$('.main_visual_box .text_wr .tit span').eq(2).addClass('active2');
+    }, 300);
+
+	$('.main_visual_box .img_wr').addClass('active');
+	
+
+}); //jQ-reday
+
+
